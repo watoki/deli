df748c7aebf01b8408ed4721e3d7bd193c05e8d8
Nikolas Martesn <nikolas.m@rtens.org>
2014-09-08 15:13:49 +0200

    Some scenarios for the RouteWithPatterns specification

--- spec/watoki/deli/RouteWithPatternsTest.php
+++ spec/watoki/deli/RouteWithPatternsTest.php
@@ -1,6 +1,20 @@
 <?php
 namespace spec\watoki\deli;
 
-class RouteWithPatternsTest {
+use watoki\scrut\Specification;
 
-} 
+/**
+ * The DynamicRouter allows partial paths and patterns which are matched against the
+ * target of the Request.
+ */
+class RouteWithPatternsTest extends Specification {
+
+    function testPatternWithPlaceholder() {
+        $this->markTestIncomplete();
+    }
+
+    function testPartialPath() {
+        $this->markTestIncomplete();
+    }
+
+}
