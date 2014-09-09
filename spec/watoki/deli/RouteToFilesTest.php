c598a3998fff2c1548bb88656207244a2e2914e1
Nikolas Martesn <nikolas.m@rtens.org>
2014-09-09 14:09:50 +0200

    Started with StaticRouter

--- spec/watoki/deli/RouteToFilesTest.php
+++ spec/watoki/deli/RouteToFilesTest.php
@@ -1,6 +1,85 @@
 <?php
 namespace spec\watoki\deli;
 
-class RouteToFilesTest {
+use spec\watoki\deli\fixtures\RequestFixture;
+use watoki\deli\router\StaticRouter;
+use watoki\deli\Target;
+use watoki\scrut\Specification;
+use watoki\stores\adapter\FileStoreAdapter;
+use watoki\stores\file\FileStore;
+use watoki\stores\file\raw\File;
+use watoki\stores\memory\MemoryStore;
+use watoki\stores\memory\SerializerRepository;
+
+/**
+ * @property RequestFixture request <-
+ */
+class RouteToFilesTest extends Specification {
+
+    function testTargetIsPlainClass() {
+        $this->givenTheClassSuffixIs('Class');
+        $this->givenTheBaseNamespaceIs('some\space');
+
+        $this->givenAClass_In_WithTheBody('some\space\foo\bar\TargetClass', 'foo/bar', '
+            function doThis() {
+                return "Found me";
+            }
+        ');
+        $this->request->givenTheRequestHasTheTarget('foo/bar/target');
+        $this->request->givenTheRequestHasTheMethod('this');
+
+        $this->whenIRouteTheRequest();
+        $this->thenTheTargetShouldRespondWith("Found me");
+    }
+
+    ###################### SET-UP #########################
+
+    private $namespace;
+
+    private $suffix;
+
+    /** @var null|Target */
+    private $target;
+
+    /** @var FileStore */
+    private $file;
+
+    protected function setUp() {
+        parent::setUp();
+        $this->file = new FileStoreAdapter(new MemoryStore(File::$CLASS, new SerializerRepository()));
+    }
+
+    private function givenTheClassSuffixIs($string) {
+        $this->suffix = $string;
+    }
+
+    private function givenTheBaseNamespaceIs($string) {
+        $this->namespace = $string;
+    }
+
+    private function givenAClass_In_WithTheBody($fullName, $folder, $body) {
+        $spaceAndName = explode('\\', $fullName);
+        $name = array_pop($spaceAndName);
+        $space = implode('\\', $spaceAndName);
+
+        $code = "namespace $space;
+            class $name {
+                $body
+            }";
+        eval($code);
+
+        $fileName = $folder . '/' . $name .'.php';
+
+        $this->file->create(new File('<?php ' . $code), $fileName);
+    }
+
+    private function whenIRouteTheRequest() {
+        $router = new StaticRouter($this->factory, $this->file, $this->namespace, $this->suffix);
+        $this->target = $router->route($this->request->request);
+    }
+
+    private function thenTheTargetShouldRespondWith($string) {
+        $this->assertEquals($string, $this->target->respond());
+    }
 
 } 
---
+++ src/watoki/deli/router/StaticRouter.php
@@ -0,0 +1,55 @@
+<?php
+namespace watoki\deli\router;
+
+use watoki\deli\Request;
+use watoki\deli\Router;
+use watoki\deli\Target;
+use watoki\deli\target\ObjectTarget;
+use watoki\factory\Factory;
+use watoki\stores\file\FileStore;
+
+class StaticRouter implements Router {
+
+    const DEFAULT_SUFFIX = 'Class';
+
+    /** @var FileStore */
+    private $store;
+
+    /** @var string */
+    private $namespace;
+
+    /** @var string */
+    private $suffix;
+
+    /** @var \watoki\factory\Factory */
+    private $factory;
+
+    function __construct(Factory $factory, FileStore $store, $namespace, $suffix = self::DEFAULT_SUFFIX) {
+        $this->factory = $factory;
+        $this->store = $store;
+        $this->namespace = $namespace;
+        $this->suffix = $suffix;
+    }
+
+    /**
+     * @param Request $request
+     * @throws \Exception
+     * @return Target
+     */
+    public function route(Request $request) {
+        $target = $request->getTarget()->copy();
+        $className = ucfirst($target->pop()) . $this->suffix;
+
+        $target->append($className);
+
+        $filePath = $target . '.php';
+
+        if ($this->store->exists($filePath)) {
+            $fullClassName = $this->namespace . '\\' . implode('\\', $target->toArray());
+            $object = $this->factory->getInstance($fullClassName);
+            return new ObjectTarget($request, $object, $this->factory);
+        }
+
+        throw new \Exception("Could not route [{$request->getTarget()}]");
+    }
+}
