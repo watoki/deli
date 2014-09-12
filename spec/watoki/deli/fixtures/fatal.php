1e934f5a82a18026aef6dcdf9ad5e05beea1c567
Nikolas Martesn <nikolas.m@rtens.org>
2014-09-12 17:55:07 +0200

    Suppress warning

--- spec/watoki/deli/fixtures/fatal.php
+++ spec/watoki/deli/fixtures/fatal.php
@@ -10,6 +10,7 @@ use watoki\deli\target\CallbackTarget;
 require_once __DIR__ . '/../../../../bootstrap.php';
 
 $router = new NoneRouter(CallbackTarget::factory(function () {
+    /** @noinspection PhpUndefinedFunctionInspection */
     causeFatalError();
 }));
 
